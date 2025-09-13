4bb9bd101f40a0b44aa69e8aaf0e2ae3a25b25ef
Gleb141 <techsengeneering@gmail.com>
2025-09-13 17:33:45 +0700

    reviewdog comments 1

--- Task_1_1_1/src/test/java/HeapsortTest.java
+++ Task_1_1_1/src/test/java/HeapsortTest.java
@@ -1,9 +1,11 @@
-import java.util.Arrays;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+
 
 /**
  * Юнит-тесты для {@link Heapsort}.
