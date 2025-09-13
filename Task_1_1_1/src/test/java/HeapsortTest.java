147a15985e2cd20a9159bfab04da5ee6fa07ba42
Gleb141 <techsengeneering@gmail.com>
2025-09-13 17:22:51 +0700

    fix imports order and avoid star import

--- Task_1_1_1/src/test/java/HeapsortTest.java
+++ Task_1_1_1/src/test/java/HeapsortTest.java
@@ -2,7 +2,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Юнит-тесты для {@link Heapsort}.
