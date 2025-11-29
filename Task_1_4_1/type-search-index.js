83ec0f036eb7892a3b713c68140704cec4aa6c87
Gleb141 <Gleb141@users.noreply.github.com>
2025-11-29 12:57:13 +0000

    Deploying to gh-pages from @ Gleb141/OOP-2-Course@c3449cbe457dd4c93d1462fff7ef5f5bcb710444 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CourseResult"},{"p":"<Unnamed>","l":"GradeBook"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
